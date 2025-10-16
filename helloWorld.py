5d0e75174a8dd717499e8abfd1f79d79efee67d3
Syed Irfan <humanic004@gmail.com>
2025-10-17 03:31:11 +0530

    some changed code

---
+++ helloWorld.py
@@ -0,0 +1,27 @@
+
+
+print("hello world")
+def myfunct():
+    print("Shut the f up")    
+myfunct()
+
+
+first_name = "Syed"
+
+print(f"{first_name}  its ok my friend")
+
+#Boolean
+is_true = True
+
+print(f"Are you a student? {is_true}")
+
+num = 23
+print(type(str(num)))
+
+name = ""
+name = bool(name)
+print(name)
+
+name = input("What is your name fellow human being!!!: ")
+
+print(f"Hello!! {name}")
