b11ac53cc8adc2d9bcc8096b9046b37a29477667
Syed Irfan <humanic004@gmail.com>
2026-05-19 23:02:17 +0530

    added new function to the class

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -9,4 +9,15 @@ class LinkedList:
     self.head = None
     self.tail = None
     
+  def add_to_head(self, new_value):
+    new_head = Node(new_value)
+    current_head = self.head_node
 
+    if current_head != None:
+      current_head.set_prev_node(new_head)
+      new_head.set_next_node(current_head)
+
+    self.head_node = new_head
+
+    if self.tail_node == None:
+      self.tail_node = new_head
